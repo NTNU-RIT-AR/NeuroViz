7a3b5f355a15a8d747704b9b53549c7b18b139c8
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2025-02-21 14:50:12 -0500

    Add ParameterSlider component - desktop app frontend

--- desktop/src/commands.ts
+++ desktop/src/commands.ts
@@ -1,16 +1,9 @@
 import { invoke } from "@tauri-apps/api/core";
 
-async function updateParameterValues(
-  s1: Number,
-  s2: Number,
-  s3: Number,
-  s4: Number
-) {
+async function updateParameterValues(slider: Number, value: Number) {
   await invoke("update_slider", {
-    newSlider1: s1,
-    newSlider2: s2,
-    newSlider3: s3,
-    newSlider4: s4,
+    sliderNumber: slider,
+    sliderValue: value,
   });
 }
 
--- desktop/src/components/ParameterSlider.tsx
+++ desktop/src/components/ParameterSlider.tsx
@@ -1,26 +1,25 @@
-// import { updateParameterValues } from "../commands";
+import { updateParameterValues } from "../commands";
 
-// export default function ParameterSlider({setSlider, slider}) {
-//   return (
-//     <>
-//       <label htmlFor="slider4">Slider 4</label>
-//       <input
-//         name="slider4"
-//         type="range"
-//         min="0"
-//         step={0.01}
-//         max="1"
-//         onChange={(e) => {
-//           e.preventDefault();
-//           updateParameterValues(
-//             slider1,
-//             slider2,
-//             slider3,
-//             Number(e.currentTarget.value)
-//           );
-//           setSlider4(parseFloat(e.currentTarget.value));
-//         }}
-//       ></input>
-//     </>
-//   );
-// }
+interface ComponentProps {
+  setSlider: (n: Number) => void;
+  slider: Number;
+}
+export default function ParameterSlider({ setSlider, slider }: ComponentProps) {
+  return (
+    <>
+      <label htmlFor={"slider" + slider}>Slider {slider.toString()}</label>
+      <input
+        name={"slider" + slider}
+        type="range"
+        min="0"
+        step={0.01}
+        max="1"
+        onChange={(e) => {
+          e.preventDefault();
+          updateParameterValues(slider, Number(e.currentTarget.value));
+          setSlider(parseFloat(e.currentTarget.value));
+        }}
+      ></input>
+    </>
+  );
+}
