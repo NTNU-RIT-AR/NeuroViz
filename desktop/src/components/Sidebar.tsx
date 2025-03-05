0335d34e0fbd33abc0d7c16fc2fe595c3d1e9cc4
Oliver Tangen <oliver@tangen.biz>
2025-03-04 19:07:28 -0500

    slider styling

--- desktop/src/components/Sidebar.tsx
+++ desktop/src/components/Sidebar.tsx
@@ -1,28 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ConnectionBox from "./ConnectionBox.tsx";
 
-import styles from "./Sidebar.module.css";
+import styles from "./styles/Sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <div className={styles.Sidebar}>
-      <ConnectionBox
-        url={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-        isConnected={true}
-      />
+      <ConnectionBox url={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"} isConnected={true} />
       <nav>
-        <Link className={styles.Link} to="/live-view">
+        <NavLink className={({ isActive }) => `${styles.Link} ${isActive ? styles.active : ""}`} to="/live-view">
           Live View
-        </Link>
-        <Link className={styles.Link} to="/presets">
+        </NavLink>
+        <NavLink className={({ isActive }) => `${styles.Link} ${isActive ? styles.active : ""}`} to="/presets">
           Presets
-        </Link>
-        <Link className={styles.Link} to="/questionaires">
+        </NavLink>
+        <NavLink className={({ isActive }) => `${styles.Link} ${isActive ? styles.active : ""}`} to="/questionaires">
           Questionaires
-        </Link>
-        <Link className={styles.Link} to="/results">
+        </NavLink>
+        <NavLink className={({ isActive }) => `${styles.Link} ${isActive ? styles.active : ""}`} to="/results">
           Results
-        </Link>
+        </NavLink>
       </nav>
     </div>
   );
