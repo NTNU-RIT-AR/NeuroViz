199009039463bd8cbc30c3c108fde8132eb8ac71
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2025-03-20 13:43:10 -0400

    Add structs for parsing data from the preset, experiment and result JSON files

--- desktop/src-tauri/src/structs.rs
+++ desktop/src-tauri/src/structs.rs
@@ -1,10 +1,11 @@
 use std::sync::{Mutex, MutexGuard};
+use std::collections::HashMap;
 
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 use tauri::{AppHandle, Manager};
 
-#[derive(Serialize, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct RenderParamsInner {
     pub hue: f32,
     pub smoothness: f32,
@@ -19,3 +20,40 @@ pub type RenderParams = Mutex<RenderParamsInner>;
 //    let state = app.state::<RenderParams>();
 //    state.lock().unwrap()
 //}
+
+#[derive(Deserialize, Serialize)]
+pub struct Choice {
+    pub a: String,
+    pub b: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Outcome {
+    pub a: String,
+    pub b: String,
+    pub selected: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Preset {
+    pub name: String,
+    pub parameters: RenderParamsInner,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Experiment {
+    pub experiment_type: String,
+    pub name: String,
+    pub presets: HashMap<String, Preset>,
+    pub choices: Vec<Choice>
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Result {
+    pub experiment_type: String,
+    pub name: String,
+    pub subject: String,
+    pub note: String,
+    pub presets: HashMap<String, Preset>,
+    pub choices: Vec<Outcome>
+}
