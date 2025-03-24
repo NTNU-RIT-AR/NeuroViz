d621517b1d4d7369d2b1bb052d6100ee7595aeed
teevik <teemu.vikoren@gmail.com>
2025-03-24 11:31:20 -0400

    RIT-40 WIP typespec

---
+++ api-spec/README.md
@@ -0,0 +1,5 @@
+# NeuroViz API Spec
+
+Source API Spec is in [./main.tsp](./main.tsp)
+
+Uses https://typespec.io/ to generate OpenAPI spec, which is then used to generate API documentation using Vite and Swagger UI.
--- api-spec/src/main.ts
+++ api-spec/src/main.ts
@@ -1,16 +1,10 @@
 import SwaggerUI from "swagger-ui";
 import SwaggerUIStandalonePreset from "swagger-ui/dist/swagger-ui-standalone-preset";
 import "swagger-ui/dist/swagger-ui.css";
-import spec from "../public/neuroviz.yaml";
+import spec from "../tsp-output/schema/openapi.yaml";
 
 SwaggerUI({
   spec: spec,
-  urls: [
-    {
-      url: "./neuroviz.yaml",
-      name: "Neuroviz",
-    },
-  ],
   dom_id: "#app",
   deepLinking: true,
   presets: [SwaggerUI.presets.apis, SwaggerUIStandalonePreset],
