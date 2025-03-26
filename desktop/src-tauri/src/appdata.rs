79f538c5973104a0b88a02af4c51a65e9d694745
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2025-03-26 11:50:06 -0400

    Manage state of tauri app, either in experiment mode or live view mode

--- desktop/src-tauri/src/appdata.rs
+++ desktop/src-tauri/src/appdata.rs
@@ -1,3 +1,5 @@
+use crate::structs::ExperimentResult;
+use crate::structs::Experiment;
 use crate::structs::RenderParams;
 use crate::structs::RenderParamsInner;
 use crate::http_server::UnityState;
@@ -9,16 +11,23 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use tokio::sync::watch;
 
+#[derive(Clone)]
+pub enum AppState {
+    ExperimentMode {experiment_result: ExperimentResult, experiment: Experiment},
+    LiveViewMode
+}
 
 #[derive(Clone)]
 pub struct AppData {
+    //TODO flytte params inn i LiveViewMode
     pub params: Arc<Mutex<RenderParamsInner>>,
-    pub watch_sender: watch::Sender<UnityState>
+    pub watch_sender: watch::Sender<UnityState>,
+    pub app_state: AppState
 }
 
 impl AppData {
     pub fn new(watch_sender: watch::Sender<UnityState>) -> Self {
-        AppData { params: Arc::new(RenderParams::default()), watch_sender: watch_sender }
+        AppData { params: Arc::new(RenderParams::default()), watch_sender: watch_sender, app_state: AppState::LiveViewMode }
     }
 
     pub fn set_params(&self, param_name: &str, value: f64){
@@ -46,5 +55,9 @@ impl AppData {
             "Emission" => params.emission,
             _ => panic!("param mismatch"),
         } as f64;
+    }
+
+    pub fn get_state(&self) -> &AppState {
+        &self.app_state
     }
 }
--- desktop/src-tauri/src/structs.rs
+++ desktop/src-tauri/src/structs.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 
+use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq)]
@@ -19,26 +20,38 @@ pub type RenderParams = Mutex<RenderParamsInner>;
 //    state.lock().unwrap()
 //}
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Choice {
     pub a: String,
     pub b: String,
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct Outcome {
-    pub a: String,
-    pub b: String,
-    pub selected: String,
-}
-
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Preset {
     pub name: String,
     pub parameters: RenderParamsInner,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize, Clone)]
+pub struct OutcomeChoice {
+    pub a: String,
+    pub b: String,
+    pub selected: String,
+    pub time: DateTime<Local>,
+    pub duration_on_a: f64,
+    pub duration_on_b: f64,
+    pub duration: f64
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct OutcomeRating {
+    pub preset: String,
+    pub rank: u8,
+    pub time: DateTime<Local>,
+    pub duration: f64
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(tag = "experiment_type")]
 pub enum ExperimentType {
     /// Rating between 1-5
@@ -50,7 +63,16 @@ pub enum ExperimentType {
     Choice { choices: Vec<Choice> },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "experiment_type")]
+pub enum ExperimentResultType {
+    #[serde(rename = "rating")]
+    Rating {ratings: Vec<OutcomeRating>},
+    #[serde(rename = "choice")]
+    Choice {choices: Vec<OutcomeChoice>}
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Experiment {
     #[serde(flatten)]
     pub experiment_type: ExperimentType,
@@ -66,13 +88,34 @@ pub struct CreateExperiment {
     pub presets: Vec<String>,
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct Result {
-    pub experiment_type: String,
+#[derive(Deserialize, Serialize, Clone)]
+pub struct ExperimentResult {
+    #[serde(flatten)]
+    pub experiment_type: ExperimentResultType,
     pub name: String,
-    pub subject: String,
+    pub time: DateTime<Local>,
+    pub observer_id: u64,
     pub note: String,
-    pub presets: HashMap<String, Preset>,
-    pub choices: Vec<Outcome>,
+    pub presets: HashMap<String, Preset>
 }
 
+impl ExperimentResult {
+    //TODO lage funksjoner for å starte opp og å avslutte eksperimentet. Når man avslutter så lagres det til fil i korresponderende folder
+    pub fn new(experiment: Experiment, observer_id: u64, note: String) -> Self {
+        Self {
+            experiment_type: match experiment.experiment_type {
+                ExperimentType::Choice {..} => ExperimentResultType::Choice { choices: Vec::new() },
+                ExperimentType::Rating => ExperimentResultType::Rating { ratings: Vec::new() }
+            },
+            name: experiment.name,
+            observer_id,
+            time: Local::now(),
+            note,
+            presets: experiment.presets
+        }
+    }
+
+    pub fn save() {
+        //TODO lagre ExperimentResult til fil
+    }
+}
