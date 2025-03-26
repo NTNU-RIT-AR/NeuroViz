b1119bf12a61f7b157645c8813419f9a53c25335
Oliver Tangen <oliver@tangen.biz>
2025-03-26 12:38:22 -0400

    temp exeriments

--- desktop/src/pages/Experiments.tsx
+++ desktop/src/pages/Experiments.tsx
@@ -1,24 +1,14 @@
 import { useEffect, useState } from "react";
 import { fetchExperiments, retrieveExperiment } from "../commands";
-import ListButton from "../components/ListButton";
 import { Layout } from "../components/Layout";
-import { ContentBox } from "../components/ContentBox";
-import CreateExperimentForm from "../components/CreateExperimentForm";
-import Button from "../components/Button";
 import { Experiment } from "../interfaces";
 import { invoke } from "@tauri-apps/api/core";
 
+import ExperimentCard from "../components/ExperimentCard";
+
+
 export default function ExperimentsPage() {
   const [experiments, setExperiments] = useState<Experiment[]>([]);
-  const [selectedExperiment, setSelectedExperiment] = useState<
-    string | undefined
-  >("");
-  const [viewCreateExperiment, setViewCreateExperiment] =
-    useState<boolean>(false);
-
-  function toggleViewCreateExperiment() {
-    setViewCreateExperiment(!viewCreateExperiment);
-  }
 
   useEffect(() => {
     invoke<Experiment[]>("get_experiments").then(setExperiments);
@@ -28,36 +18,31 @@ export default function ExperimentsPage() {
     <>
       <h1>Experiments</h1>
       <Layout>
-        <ContentBox>
-          {experiments.length > 0 &&
-            experiments.map((experiment_name) => (
-              <ListButton
-                textContent={experiment_name}
-                onClick={() => {
-                  retrieveExperiment(experiment_name).then((result) =>
-                    setSelectedExperiment(result)
-                  );
-                }}
-              />
-            ))}
-        </ContentBox>
+        {
+          experiments.map((experiment) => (
+            <ExperimentCard data={experiment} />
 
-        <ContentBox>{selectedExperiment && selectedExperiment}</ContentBox>
+            // retrieveExperiment(experiment_name).then((result) =>
+            //   setSelectedExperiment(result)
+          ))
+        }
+
       </Layout>
-      {viewCreateExperiment && <CreateExperimentForm />}
-      {viewCreateExperiment ? (
-        <Button
-          theme={"neutral"}
-          textContent="Cancel"
-          onClick={toggleViewCreateExperiment}
-        />
-      ) : (
-        <Button
-          theme={"green"}
-          textContent="Create new"
-          onClick={toggleViewCreateExperiment}
-        />
-      )}
+
+      {/* {viewCreateExperiment && <CreateExperimentForm />} */}
+      {/* {viewCreateExperiment ? ( */}
+      {/*   <Button */}
+      {/*     theme={"neutral"} */}
+      {/*     textContent="Cancel" */}
+      {/*     onClick={toggleViewCreateExperiment} */}
+      {/*   /> */}
+      {/* ) : ( */}
+      {/*   <Button */}
+      {/*     theme={"green"} */}
+      {/*     textContent="Create new" */}
+      {/*     onClick={toggleViewCreateExperiment} */}
+      {/*   /> */}
+      {/* )} */}
     </>
   );
 }
