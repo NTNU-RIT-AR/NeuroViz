5b8c6ab7e909bd1b88e88ac13c5a9f7f3f8c7e60
Oliver Tangen <oliver@tangen.biz>
2025-03-27 09:52:17 -0400

    fix merge bug

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,19 +1,17 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 pub mod api;
+pub mod appdata;
 pub mod consts;
 pub mod structs;
-pub mod appdata;
 
-use appdata::AppData;
 use api::{commands, http_server};
+use appdata::AppData;
 
 use tauri::Manager;
 use tokio::sync::watch;
 
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
@@ -21,13 +19,10 @@ pub fn run() {
             commands::get_param,
             commands::get_ip_address,
             commands::list_presets,
-
             commands::get_preset,
             commands::create_preset,
-
             commands::get_experiment,
-            commands::create_experiment
-
+            commands::create_experiment,
             commands::list_experiments,
             commands::get_all_experiments,
             commands::start_experiment
@@ -44,7 +39,7 @@ pub fn run() {
             tauri::async_runtime::spawn(http_server.run());
 
             // println!("{:?}", commands::start_experiment(app.handle().clone(), String::from("example-experiment-1"), 0, String::from("my note hihi")) );
-            
+
             Ok(())
         })
         .run(tauri::generate_context!())
