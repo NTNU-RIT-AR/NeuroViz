a903fd9854200a000b57e48eddc9eccb8ac9b5fe
teevik <teemu.vikoren@gmail.com>
2025-03-28 17:50:00 -0400

    Refactor get_current_preset and swap_current_preset

--- desktop/src-tauri/src/appdata.rs
+++ desktop/src-tauri/src/appdata.rs
@@ -1,6 +1,8 @@
-use crate::structs::{UnityExperimentType, RenderParamsInner, RenderParams, Preset, ExperimentType, Experiment, ExperimentState, ExperimentResult, ExperimentPrompt, CurrentPreset};
 use crate::http_server::UnityState;
-
+use crate::structs::{
+    CurrentPreset, Experiment, ExperimentPrompt, ExperimentResult, ExperimentState, ExperimentType,
+    Preset, RenderParams, RenderParamsInner, UnityExperimentType,
+};
 
 use crate::api::{events, http_server};
 
@@ -12,8 +14,12 @@ use tokio::sync::watch;
 
 #[derive(Clone)]
 pub enum AppState {
-    ExperimentMode {experiment_result: ExperimentResult, experiment: Experiment, experiment_state: ExperimentState},
-    LiveViewMode
+    ExperimentMode {
+        experiment_result: ExperimentResult,
+        experiment: Experiment,
+        experiment_state: ExperimentState,
+    },
+    LiveViewMode,
 }
 
 #[derive(Clone)]
@@ -22,15 +28,20 @@ pub struct AppData {
     pub params: Arc<Mutex<RenderParamsInner>>,
     pub watch_sender: watch::Sender<UnityState>,
     pub app_state: Arc<Mutex<AppState>>,
-    pub app_handle: AppHandle
+    pub app_handle: AppHandle,
 }
 
 impl AppData {
     pub fn new(watch_sender: watch::Sender<UnityState>, app_handle: AppHandle) -> Self {
-        AppData { params: Arc::new(RenderParams::default()), watch_sender, app_state: Arc::new(Mutex::new(AppState::LiveViewMode)), app_handle }
+        AppData {
+            params: Arc::new(RenderParams::default()),
+            watch_sender,
+            app_state: Arc::new(Mutex::new(AppState::LiveViewMode)),
+            app_handle,
+        }
     }
 
-    pub fn set_param(&self, param_name: &str, value: f64){
+    pub fn set_param(&self, param_name: &str, value: f64) {
         let value = value as f32;
         let mut params = self.params.lock().unwrap();
         println!("Updated slider values {:?}", params);
@@ -42,7 +53,11 @@ impl AppData {
             _ => {}
         }
 
-        self.watch_sender.send(http_server::UnityState::Live { parameters: params.clone() }).unwrap();
+        self.watch_sender
+            .send(http_server::UnityState::Live {
+                parameters: params.clone(),
+            })
+            .unwrap();
     }
 
     pub fn get_param(&self, param_name: &str) -> f64 {
@@ -67,42 +82,83 @@ impl AppData {
         (*app_state) = new_state;
     }
 
-    pub fn get_current_preset(&self, swap: Option<()>) -> Result<Preset, String> {
-        let mut app_state = self.app_state.lock().unwrap();
-        let app_state = &mut *app_state;
-        //Check if current state is AppState::ExperimentMode
+    pub fn get_current_preset(&self) -> Result<Preset, String> {
+        let app_state = self.app_state.lock().unwrap();
 
-        let AppState::ExperimentMode { experiment_result: _, experiment, experiment_state } = app_state else {
-            return Err(String::from("Can not get current preset when in LiveViewMode"));
+        // Check if current state is AppState::ExperimentMode
+        let AppState::ExperimentMode {
+            experiment,
+            experiment_state,
+            ..
+        } = &*app_state
+        else {
+            return Err(String::from(
+                "Can not get current preset when in LiveViewMode",
+            ));
         };
 
-        match &mut experiment.experiment_type {
-            ExperimentType::Choice {choices} => return match (&mut experiment_state.choice_current_preset, swap) {
-                (CurrentPreset::A, None) | (CurrentPreset::B, Some(_)) => {
-                    if swap == Some(()) {
-                        experiment_state.choice_current_preset = CurrentPreset::A;
-                    }
-                    Ok(experiment.presets[&choices[experiment_state.current_index].a].clone())
-                },
-                (CurrentPreset::B, None) | (CurrentPreset::A, Some(_)) => {
-                    if swap == Some(()) {
-                        experiment_state.choice_current_preset = CurrentPreset::B;
-                    }
-                    Ok(experiment.presets[&choices[experiment_state.current_index].b].clone())
+        let preset = match &experiment.experiment_type {
+            ExperimentType::Choice { choices } => {
+                let choice = &choices[experiment_state.current_index];
+
+                match experiment_state.choice_current_preset {
+                    CurrentPreset::A => experiment.presets[&choice.a].clone(),
+                    CurrentPreset::B => experiment.presets[&choice.b].clone(),
                 }
-            },
-            ExperimentType::Rating { order } => return Ok(experiment.presets[&order[experiment_state.current_index]].clone())
+            }
+
+            ExperimentType::Rating { order } => {
+                let preset_name = &order[experiment_state.current_index];
+
+                experiment.presets[preset_name].clone()
+            }
         };
+
+        Ok(preset)
     }
 
-    pub fn swap_current_preset(&self) -> Result<Preset, String> {
-        match self.get_current_preset(Some(())) {
-            Ok(preset) => {
-                events::emit_swap_preset_in_experiment(&self.app_handle, &preset);
-                self.watch_sender.send(http_server::UnityState::Experiment { prompt: ExperimentPrompt {experiment_type: UnityExperimentType::Choice, preset: preset.clone()} }).unwrap();
-                Ok(preset)
-            },
-            Err(e) => Err(e)
-        }
+    pub fn swap_current_preset(&self) -> Result<(), String> {
+        let mut app_state = self.app_state.lock().unwrap();
+
+        // Check if current state is AppState::ExperimentMode
+        let AppState::ExperimentMode {
+            experiment,
+            experiment_state,
+            ..
+        } = &mut *app_state
+        else {
+            return Err(String::from(
+                "Can not get current preset when in LiveViewMode",
+            ));
+        };
+
+        let ExperimentType::Choice { choices } = &mut experiment.experiment_type else {
+            return Err(String::from("Can not swap preset in Rating experiment"));
+        };
+
+        let choice = &choices[experiment_state.current_index];
+        let preset = match experiment_state.choice_current_preset {
+            CurrentPreset::A => {
+                experiment_state.choice_current_preset = CurrentPreset::B;
+                experiment.presets[&choice.b].clone()
+            }
+            CurrentPreset::B => {
+                experiment_state.choice_current_preset = CurrentPreset::A;
+                experiment.presets[&choice.a].clone()
+            }
+        };
+
+        // Send events
+        events::emit_swap_preset_in_experiment(&self.app_handle, &preset);
+        self.watch_sender
+            .send(http_server::UnityState::Experiment {
+                prompt: ExperimentPrompt {
+                    experiment_type: UnityExperimentType::Choice,
+                    preset: preset.clone(),
+                },
+            })
+            .unwrap();
+
+        Ok(())
     }
 }
